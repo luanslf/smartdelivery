854b873e1089763a43a8ad293438cc57ba9dbd03
luanslf <luaanx05@gmail.com>
2025-12-09 19:29:54 -0300

    feat: initial commit for first spring boot project

---
+++ src/test/java/br/com/javadevweek/smartdelivery/SmartdeliveryApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.javadevweek.smartdelivery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartdeliveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
